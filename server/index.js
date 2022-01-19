e4915782e79fb99415fb91950fcc1b15ab372efd
mouauia <benaissamouauia7@gmail.com>
2022-01-19 21:12:36 +0100

    add jenkinsfile

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb://localhost:27017/test";
+const CONNECTION_URL = "mongodb://mongo:27017/test";
 const PORT = process.env.PORT || 5000;
 
 mongoose
